efc13a16a359fce3f4d571128d17fcab800e03ce
Katerina Solovyova <yekaterina.solovyova@gmail.com>
2020-08-29 12:38:31 -0400

    up to delete a reservation

--- src/index.js
+++ src/index.js
@@ -11,14 +11,19 @@ class App extends Component{
       userId: '', // 1, 2 or 3
       restaurants: [],
       reservations: []
-    };
+    }
+    this.reservationCount = this.reservationCount.bind(this)
+    this.makeReservation = this.makeReservation.bind(this)
+    this.destroyReservation = this.destroyReservation.bind(this)
   }
+
   async componentDidMount(){
     const users = (await axios.get('/api/users')).data;
     this.setState({ users });
     const restaurants = (await axios.get('/api/restaurants')).data
     this.setState({restaurants});
     makeMap('map');
+
     const loadReservations = async()=> {
       const userId = window.location.hash.slice(1) * 1;
       const reservations = (await axios.get(`/api/users/${userId}/reservations`)).data
@@ -30,6 +35,7 @@ class App extends Component{
       this.setState({ reservations: restaurantNames, userId})
     };
     window.addEventListener('hashchange', async()=> {
+      //console.log(this.state.reservations)
       loadReservations();
     });
     if(window.location.hash.slice(1)){
@@ -39,6 +45,41 @@ class App extends Component{
       this.setState({ userId: users[0].id });
     }
   }
+
+  reservationCount (restaurantName) {
+    let count = 0
+    for (let i=0; i<this.state.reservations.length; i++) {
+      let currObject = this.state.reservations[i]
+      let currRestaurantName = currObject.name
+      if (restaurantName === currRestaurantName) {
+        count++
+      }
+    }
+    return count
+  }
+
+  async makeReservation (userId,restaurantId) {
+    await axios.post(`/api/users/${userId}/reservations`,{restaurantId: restaurantId})
+    const reservations = (await axios.get(`/api/users/${userId}/reservations`)).data
+      const restaurantNames = reservations.map(reservation => {
+        return this.state.restaurants.find(restaurant => {
+          return restaurant.id === reservation.restaurantId
+        })
+      })
+    this.setState({ reservations: restaurantNames, userId})
+  }
+
+  async destroyReservation (reservationId) {
+    await axios.delete(`/api/reservations/${reservationId}`)
+    const reservations = (await axios.get(`/api/users/${this.state.userId}/reservations`)).data
+      const restaurantNames = reservations.map(reservation => {
+        return this.state.restaurants.find(restaurant => {
+          return restaurant.id === reservation.restaurantId
+        })
+      })
+    this.setState({ reservations: restaurantNames, userId: this.state.userId})
+  }
+
   render(){
     const { users, userId , restaurants, reservations} = this.state;
     return (
@@ -65,7 +106,11 @@ class App extends Component{
               {
                 restaurants.map(restaurant => {
                   return (
-                    <li key = {restaurant.id} className={ restaurants.id }>{restaurant.name}</li>
+                  <li key = {restaurant.id} className={ restaurants.id }>
+                    <a onClick = {() => this.makeReservation(this.state.userId,restaurant.id)}>
+                      {restaurant.name} {this.reservationCount(restaurant.name)}
+                    </a>
+                  </li>
                   )
                 })
               }
@@ -76,7 +121,9 @@ class App extends Component{
               {
                 reservations.map(reservation => {
                   return (
-                  <li key = {reservation.id} className = {reservations.id }>{reservation.name} ({reservation.createdAt}</li>
+                  <li key = {reservation.id} className = {reservations.id }>
+                    <a onClick = {() => this.destroyReservation(reservation.id)}>{reservation.name} ({reservation.createdAt})</a>
+                  </li>
                   )
                 })
               }
